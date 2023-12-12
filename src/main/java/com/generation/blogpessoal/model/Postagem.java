2071469721adf3e68e43aaffb6bfe82f3df91517
felipe-jaque-dev <felipealexandrej@gmail.com>
2023-12-12 18:14:25 -0300

    Adicionado Interface Tema e Controller Tema

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,11 +4,14 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -37,6 +40,10 @@ public class Postagem {
 	@UpdateTimestamp
 	private LocalDateTime data;
 
+	@ManyToOne// Muitas postagens para um tema
+	@JsonIgnoreProperties("postagem") // Evita a recursividade, ignorando a propria classe
+	private Tema tema;
+	
     public Long getId() {
         return this.id;
     }
@@ -68,5 +75,14 @@ public class Postagem {
     public void setData(LocalDateTime data) {
         this.data = data;
     }
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
   
+    
 }
